99c9c7d0ba8a16b171e0936cfc641b37bd6580da
migueljr <miguel_jr@mail.com>
2023-03-15 16:04:11 -0300

    ajustes

--- video-play.js
+++ video-play.js
@@ -1,5 +1,5 @@
 const videoPlayer = document.getElementById("videoPlayer");
-const videoSource = "http://example.com/example.m3u8"; // Substitua pelo seu arquivo M3U8
+const videoSource = "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"; // Substitua pelo seu arquivo M3U8
 
 if (Hls.isSupported()) {
   const hls = new Hls();
